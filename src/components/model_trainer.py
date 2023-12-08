6f92f3ef16602b31676875c47dd69a30b7794140
Vidhya V <vidhyakmp2005@gmail.com>
2023-12-08 08:56:35 +0530

    model trainer 3

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -28,7 +28,7 @@ class ModelTrainer:
         self.model_trainer_config=ModelTrainerConfig()
 
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self ,train_array ,test_array):
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
